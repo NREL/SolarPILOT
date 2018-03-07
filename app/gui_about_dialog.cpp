c08982101e0f3a90d3d9f5eade7edc6593cdd5cd
Mike Wagner <mjwagner2@gmail.com>
2018-03-07 15:26:52 -0600

    license header

--- app/gui_about_dialog.cpp
+++ app/gui_about_dialog.cpp
@@ -1,3 +1,54 @@
+/*******************************************************************************************************
+*  Copyright 2018 Alliance for Sustainable Energy, LLC
+*
+*  NOTICE: This software was developed at least in part by Alliance for Sustainable Energy, LLC
+*  ("Alliance") under Contract No. DE-AC36-08GO28308 with the U.S. Department of Energy and the U.S.
+*  The Government retains for itself and others acting on its behalf a nonexclusive, paid-up,
+*  irrevocable worldwide license in the software to reproduce, prepare derivative works, distribute
+*  copies to the public, perform publicly and display publicly, and to permit others to do so.
+*
+*  Redistribution and use in source and binary forms, with or without modification, are permitted
+*  provided that the following conditions are met:
+*
+*  1. Redistributions of source code must retain the above copyright notice, the above government
+*  rights notice, this list of conditions and the following disclaimer.
+*
+*  2. Redistributions in binary form must reproduce the above copyright notice, the above government
+*  rights notice, this list of conditions and the following disclaimer in the documentation and/or
+*  other materials provided with the distribution.
+*
+*  3. The entire corresponding source code of any redistribution, with or without modification, by a
+*  research entity, including but not limited to any contracting manager/operator of a United States
+*  National Laboratory, any institution of higher learning, and any non-profit organization, must be
+*  made publicly available under this license for as long as the redistribution is made available by
+*  the research entity.
+*
+*  4. Redistribution of this software, without modification, must refer to the software by the same
+*  designation. Redistribution of a modified version of this software (i) may not refer to the modified
+*  version by the same designation, or by any confusingly similar designation, and (ii) must refer to
+*  the underlying software originally provided by Alliance as "Solar Power tower Integrated Layout and 
+*  Optimization Tool" or "SolarPILOT". Except to comply with the foregoing, the terms "Solar Power 
+*  tower Integrated Layout and Optimization Tool", "SolarPILOT", or any confusingly similar
+*  designation may not be used to refer to any modified version of this software or any modified
+*  version of the underlying software originally provided by Alliance without the prior written consent
+*  of Alliance.
+*
+*  5. The name of the copyright holder, contributors, the United States Government, the United States
+*  Department of Energy, or any of their employees may not be used to endorse or promote products
+*  derived from this software without specific prior written permission.
+*
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+*  IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+*  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER,
+*  CONTRIBUTORS, UNITED STATES GOVERNMENT OR UNITED STATES DEPARTMENT OF ENERGY, NOR ANY OF THEIR
+*  EMPLOYEES, BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+*  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+*  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+*  IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+*  THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************************************/
+
+
 #include "gui_about_dialog.h"
 #include <wx/clipbrd.h>
 #include <wx/dcbuffer.h>
@@ -59,27 +110,55 @@ gui_license::gui_license(wxWindow *parent, wxWindowID id, wxIcon icon)
     SetIcon( icon );
 
     wxPanel *main_panel = new wxPanel(this);
-    _lic_msg = "Copyright © 2013 the Alliance for Sustainable Energy, LLC, All Rights Reserved\n\n"
-        "This computer software is prepared by the Alliance for Sustainable Energy, LLC, (hereinafter the \"Contractor\"), "
-        "under Contract DE-AC36-08G028308 (\"Contract\") with the Department of Energy (\"DOE\"). "
-        "The United States Government has been granted for itself and others acting on its behalf a paid-up, "
-        "non-exclusive, irrevocable, worldwide license in the Software to reproduce, prepare derivative works, "
-        "and perform publicly and display publicly. Beginning five (5) years after the date permission to assert "
-        "copyright is obtained from DOE, and subject to any subsequent five (5) year renewals, the United States "
-        "Government is granted for itself and others acting on its behalf a paid-up, non-exclusive, irrevocable, "
-        "worldwide license in the Software to reproduce, prepare derivative works, distribute copies to the public, "
-        "perform publicly and display publicly, and to permit others to do so. If the Contractor ceases to make "
-        "this computer software available, it may be obtained from DOE's Office of Scientific and Technical "
-        "Information's Energy Science and Technology Software Center (ESTSC) at PO Box 1020, Oak Ridge, "
-        "TN 37831-1020. THIS SOFTWARE IS PROVIDED BY THE CONTRACTOR \"AS IS\" AND ANY EXPRESS OR IMPLIED "
-        "WARRANTIES, INCLUDING BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS "
-        "FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE CONTRACTOR, DOE, OR THE U.S GOVERNMENT "
-        "BE LIABLE FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER, INCLUDING BUT "
-        "NOT LIMITED TO CLAIMS ASSOCIATED WITH THE LOSS OF DATA OR PROFITS, WHICH MAY RESULT FROM AN ACTION IN "
-        "CONTRACT, NEGLIGENCE OR OTHER TORTIOUS CLAIM THAT ARISES OUT OF OR IN CONNECTION WITH THE ACCESS, "
-        "USE OR PERFORMANCE OF THIS SOFTWARE.\n\n"
-        "The Software is being provided for internal purposes only and may not be re-distributed. "
-        "Use of this software implies acceptance of this license agreement.";
+    _lic_msg = 
+		"Copyright 2018 Alliance for Sustainable Energy, LLC "
+		"\n\n"
+		"NOTICE: This software was developed at least in part by Alliance for Sustainable Energy, LLC "
+		"(\"Alliance\") under Contract No. DE-AC36-08GO28308 with the U.S. Department of Energy and the U.S. "
+		"The Government retains for itself and others acting on its behalf a nonexclusive, paid-up, "
+		"irrevocable worldwide license in the software to reproduce, prepare derivative works, distribute "
+		"copies to the public, perform publicly and display publicly, and to permit others to do so. "
+		"\n\n"
+		"Redistribution and use in source and binary forms, with or without modification, are permitted "
+		"provided that the following conditions are met: "
+		"\n\n"
+		"1. Redistributions of source code must retain the above copyright notice, the above government "
+		"rights notice, this list of conditions and the following disclaimer. "
+		"\n\n"
+		"2. Redistributions in binary form must reproduce the above copyright notice, the above government "
+		"rights notice, this list of conditions and the following disclaimer in the documentation and/or "
+		"other materials provided with the distribution. "
+		"\n\n"
+		"3. The entire corresponding source code of any redistribution, with or without modification, by a "
+		"research entity, including but not limited to any contracting manager/operator of a United States "
+		"National Laboratory, any institution of higher learning, and any non-profit organization, must be "
+		"made publicly available under this license for as long as the redistribution is made available by "
+		"the research entity. "
+		"\n\n"
+		"4. Redistribution of this software, without modification, must refer to the software by the same "
+		"designation. Redistribution of a modified version of this software (i) may not refer to the modified "
+		"version by the same designation, or by any confusingly similar designation, and (ii) must refer to "
+		"the underlying software originally provided by Alliance as \"Solar Power tower Integrated Layout and "
+		"Optimization Tool\" or \"SolarPILOT\".Except to comply with the foregoing, the terms \"Solar Power "
+		"tower Integrated Layout and Optimization Tool\", \"SolarPILOT\", or any confusingly similar "
+		"designation may not be used to refer to any modified version of this software or any modified "
+		"version of the underlying software originally provided by Alliance without the prior written consent "
+		"of Alliance. "
+		"\n\n"
+		"5. The name of the copyright holder, contributors, the United States Government, the United States "
+		"Department of Energy, or any of their employees may not be used to endorse or promote products "
+		"derived from this software without specific prior written permission. "
+		"\n\n"
+		"THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS \"AS IS\" AND ANY EXPRESS OR "
+		"IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND "
+		"FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER, "
+		"CONTRIBUTORS, UNITED STATES GOVERNMENT OR UNITED STATES DEPARTMENT OF ENERGY, NOR ANY OF THEIR "
+		"EMPLOYEES, BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL "
+		"DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, "
+		"DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER "
+		"IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF "
+		"THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
+		;
 
     wxTextCtrl *tcmsg = new wxTextCtrl(main_panel, wxID_ANY, _lic_msg, wxDefaultPosition, wxSize(700,350), wxTE_MULTILINE|wxTE_READONLY|wxTE_RICH2);
     wxButton *okbut = new wxButton(main_panel, wxID_ANY, "Done");
