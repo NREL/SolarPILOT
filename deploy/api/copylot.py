bf431e63a3d30424c2bf1a5bcf1ee2c9fcd13828
WHamilt2 <william.hamilton@nrel.gov>
2020-06-05 12:38:18 -0600

    added more functionality through python API - have 5 more functions to set up

--- deploy/api/copylot.py
+++ deploy/api/copylot.py
@@ -1,4 +1,5 @@
 import string, sys, os
+import pandas as pd
 from ctypes import *
 c_number = c_double   #must be either c_double or c_float depending on copilot.h definition
 
@@ -50,6 +51,15 @@ class CoPylot:
         arr[:] = parr # set all at once
         return self.pdll.sp_set_array(c_void_p(p_data), c_char_p(name), pointer(arr), c_int(count))
 
+    #Sets array using csv - TODO: test to see if it works
+    def data_set_array_from_csv(self, p_data, name, fn):
+        f = open(fn, 'r')
+        data = []
+        for line in f:
+            data.extend([n for n in map(float, line.split(','))])
+        f.close()
+        return self.data_set_array(p_data, name, data)
+
     #SPEXPORT void sp_set_matrix(sp_data_t p_data, const char *name, sp_number_t *pvalues, int nrows, int ncols)
     def data_set_matrix(self, p_data, name, mat):
         nrows = len(mat)
@@ -82,7 +92,6 @@ class CoPylot:
     def data_get_string(self, p_data, name):
         self.pdll.sp_get_string.restype = c_char_p
         return self.pdll.sp_get_string(c_void_p(p_data), c_char_p(name))
-        # TODO: returning '' I am using the same format as SCC.  Have Mike check to see if the scc function works.
 
     #SPEXPORT sp_number_t *sp_get_array(sp_data_t p_data, const char *name, int *length)
     def data_get_array(self, p_data, name):
@@ -91,15 +100,6 @@ class CoPylot:
         parr = self.pdll.sp_get_array(c_void_p(p_data), c_char_p(name), byref(count))
         arr = parr[0:count.value]
         return arr
-
-    #Sets array using csv - TODO: test to see if it works
-    def data_set_array_from_csv(self, p_data, name, fn):
-        f = open(fn, 'r')
-        data = []
-        for line in f:
-            data.extend([n for n in map(float, line.split(','))])
-        f.close()
-        return self.data_set_array(p_data, name, data)
 
     #SPEXPORT sp_number_t *sp_get_matrix(sp_data_t p_data, const char *name, int *nrows, int *ncols)
     def data_get_matrix(self,p_data,name):
@@ -139,10 +139,165 @@ class CoPylot:
         self.pdll.sp_drop_heliostat_template.restype = c_int
         return self.pdll.sp_drop_heliostat_template( c_void_p(p_data), c_char_p(helio_name))
 
-        
+    #SPEXPORT int sp_update_geometry(sp_data_t p_data)
+    def update_geometry(self, p_data):
+        self.pdll.sp_update_geometry.restype = c_int
+        return self.pdll.sp_update_geometry( c_void_p(p_data))
 
+    #SPEXPORT bool sp_generate_layout(sp_data_t p_data, int nthreads = 0)
+    def generate_layout(self, p_data, nthreads = 0):
+        self.pdll.sp_generate_layout.restype = c_bool
+        return self.pdll.sp_generate_layout( c_void_p(p_data), c_int(nthreads))
+
+    #SPEXPORT bool sp_assign_layout(sp_data_t p_data, sp_number_t* pvalues, int nrows, int ncols, int nthreads = 0) //, bool save_detail = true)
+    def assign_layout(self, p_data, helio_data, nthreads = 0):
+        nrows = len(helio_data)
+        ncols = len(helio_data[0])
+        size = nrows*ncols
+        arr = (c_number*size)()
+        idx = 0
+        for r in range(nrows):
+            for c in range(ncols):
+                arr[idx] = c_number(helio_data[r][c])
+                idx += 1
+        self.pdll.sp_assign_layout.restype = c_bool
+        return self.pdll.sp_assign_layout( c_void_p(p_data), pointer(arr), c_int(nrows), c_int(ncols), c_int(nthreads))
+
+    #SPEXPORT sp_number_t* sp_get_layout_info(sp_data_t p_data, int* nhelio, int* ncol)
+    def get_layout_info(self, p_data):
+        nrows = c_int()
+        ncols = c_int()
+        self.pdll.sp_get_layout_info.restype = POINTER(c_number)
+        parr = self.pdll.sp_get_layout_info( c_void_p(p_data), byref(nrows), byref(ncols) )
+        mat = []
+        for r in range(nrows.value):
+            row = []
+            for c in range(ncols.value):
+                row.append( float(parr[ncols.value * r + c]))
+            mat.append(row)
+        return mat
+
+    #SPEXPORT bool sp_simulate(sp_data_t p_data, int nthreads = 1, bool save_detail = true, bool update_aimpoints = true)
+    def simulate(self, p_data, nthreads = 1, save_detail = True, update_aimpoints = True):
+        # TODO: Test function
+        self.pdll.sp_simulate.restype = c_bool
+        return self.pdll.sp_simulate( c_void_p(p_data), c_int(nthreads), c_bool(save_detail), c_bool(update_aimpoints))
+
+    #SPEXPORT const char *sp_summary_results(sp_data_t p_data)
+    def summary_results(self, p_data, save_dict = False):
+        # TODO: Test function
+        self.pdll.sp_summary_results.restype = c_char_p
+        ret = self.pdll.sp_summary_results( c_void_p(p_data)).decode()
+        # save result table to dictionary
+        if save_dict:
+            items = ret.split('\n')
+            res_dict = {}
+            for i in items:
+                key_val = i.split(',')
+                try:
+                    res_dict[key_val[0]] = float(key_val[1])
+                except:
+                    res_dict[key_val[0]] = key_val[1]
+            return res_dict
+        else:    # print results table
+            return print(ret)
+
+    #SPEXPORT sp_number_t* sp_detail_results(sp_data_t p_data, int* nrows, int* ncols, sp_number_t* selhel = NULL, int nselhel = 0)
+    def detail_results(self, p_data, selhel = None, restype = "dataframe"):
+        # handling selected heliostats
+        if selhel == None:
+            nselhel = 0
+            arr = c_number(-100)
+        else:
+            nselhel = len(selhel)
+            arr = (c_number*nselhel)()
+            arr[:] = selhel # set all at once
+
+        nrows = c_int()
+        ncols = c_int()
+        # Get data
+        self.pdll.sp_detail_results.restype = POINTER(c_number)
+        res_arr = self.pdll.sp_detail_results( c_void_p(p_data), byref(nrows), byref(ncols), pointer(arr), c_int(nselhel))
+        # Get header
+        self.pdll.sp_detail_results_header.restype = c_char_p
+        header = self.pdll.sp_detail_results_header().decode()
+        header = header.split(',')
+        if restype.lower().startswith("mat"):
+            # output matrix
+            mat = []
+            for r in range(nrows.value):
+                row = []
+                for c in range(ncols.value):
+                    row.append( float(res_arr[ncols.value * r + c]))
+                mat.append(row)
+            return mat, header
+        elif restype.lower().startswith("dic") or restype.lower().startswith("dat"):
+            # output dictionary
+            ret = {}
+            for c,key in enumerate(header):
+                ret[key] = []
+                for r in range(nrows.value):
+                    ret[key].append( float(res_arr[ncols.value * r + c]))
+            if restype.lower().startswith("dic"):
+                return ret
+            else:
+                df = pd.DataFrame(ret)
+                df.set_index(header[0])
+                return df
+
+    #SPEXPORT bool sp_get_fluxmap(sp_data_t p_data, sp_number_t* fluxmap, int* nrows, int* ncols, int rec_id = 0)
+    def get_fluxmap(self, p_data, rec_id = 0):
+        # TODO: set up function
+        pass
+    
+    #SPEXPORT void sp_clear_land(sp_data_t p_data, const char* type = NULL)
+    def clear_land(self, p_data, clear_type = None):
+        ## TODO: Test function
+        ## TODO: Should add a bool to return if the process was successful or not
+        return self.pdll.sp_clear_land( c_void_p(p_data), c_char_p(clear_type))
+
+    #SPEXPORT bool sp_add_land(sp_data_t p_data, const char* type, sp_number_t* polygon_points, int* npts , int* ndim, bool is_append = true)
+    def add_land(self, p_data, add_type, poly_points, is_append = True):
+        ## TODO: set-up and test function
+        npts = len(poly_points)
+        ndim = len(poly_points[0])
+        size = npts*ndim
+        PParr = (c_number*size)()
+        idx = 0
+        for r in range(npts):
+            for c in range(ndim):
+                PParr[idx] = c_number(poly_points[r][c])
+                idx += 1
+        
+        self.pdll.sp_add_land.restype = c_bool
+        return self.pdll.sp_add_land( c_void_p(p_data), c_char_p(add_type), pointer(PParr), c_int(npts), c_int(ndim), c_bool(is_append))
+
+    #SPEXPORT bool sp_heliostats_by_region(sp_data_t p_data, sp_number_t* retvec, int* lenret, const char* coor_sys,
+    #                                         sp_number_t* arguments = NULL, int* len_arg = NULL, 
+    #                                         const char* svgfname_data = NULL, sp_number_t* svg_opt_tab = NULL)
+    def heliostats_by_region(self, p_data):
+        # TODO: set-up and test function
+        pass
+
+    #SPEXPORT bool sp_modify_heliostats(sp_data_t p_data, sp_number_t* helio_data, int* nhel, int* ncols, const char* table_hdr)
+    def modify_heliostats(self, p_data, helio_data, table_hdr):
+        # TODO: set-up and test function
+        pass
+
+    #SPEXPORT bool sp_save_from_script(sp_data_t p_data, const char* sp_fname)
+    def save_from_script(self, p_data, sp_fname):
+        self.pdll.sp_save_from_script.restype = c_bool
+        return self.pdll.sp_save_from_script( c_void_p(p_data), c_char_p(sp_fname))
+
+    #SPEXPORT bool sp_dump_varmap(sp_data_t p_data, const char* sp_fname)
+    def dump_varmap_tofile(self, p_data, sp_fname):
+        self.pdll.sp_dump_varmap.restype = c_bool
+        return self.pdll.sp_dump_varmap( c_void_p(p_data), c_char_p(sp_fname))
+    
 #-----------------------------------------------------------------
 if __name__ == "__main__":
+
+    
 
     cp = CoPylot()
 
@@ -159,7 +314,8 @@ if __name__ == "__main__":
     ## Testing set and get string - TODO
     if False:
         cp.data_set_string(r, b"ambient.0.loc_state", b"Mind")  #- Working
-        print ( cp.data_get_string(r, b"ambient.0.loc_state"))
+        print ( cp.data_get_string(r, b"ambient.0.loc_state")) #- Working TODO: Talk to Mike about strdup()
+        # TODO: had to also add it to summary_results
 
     ## Testing set and get array - DONE
     if False:
@@ -179,20 +335,96 @@ if __name__ == "__main__":
         print( cp.data_get_array(r, b"financial.0.pmt_factors"))
         print( cp.data_get_matrix(r, b"ambient.0.atm_coefs"))
 
-    ## Testing add and drop receiver
+    ## Testing add and drop receiver - DONE
     if False:
         print( cp.add_receiver(r, b"test_rec"))
         print( cp.add_receiver(r, b"test_rec"))
         print( cp.drop_receiver(r, b"test_rec"))
         print( cp.drop_receiver(r, b"test_re"))
 
-    ## Testing add and drop heliostat template
-    if True:
+    ## Testing add and drop heliostat template - DONE
+    if False:
         print( cp.add_heliostat_template(r, b"test_helio"))
         print( cp.add_heliostat_template(r, b"test_helio"))
         print( cp.drop_heliostat_template(r, b"test_helio"))
         print( cp.drop_heliostat_template(r, b"test_heli"))
 
+    ## Testing setting and getting Combo variables - TODO - We should improve this function
+            # are all combos strings?
+    if False:
+        print (cp.data_get_string(r, b"ambient.0.sun_type"))
+        cp.data_set_string(r, b"ambient.0.sun_type" , b'Gaussian su') # - Working now
+        print (cp.data_get_string(r, b"ambient.0.sun_type"))
+
+    ## Testing generate, assign - DONE, and update layout - TODO:
+    if True:
+        cp.reset_vars(r)
+
+        # generate
+        if True:
+            cp.data_set_string(r, b"solarfield.0.des_sim_detail", b"Do not filter heliostats")
+            ## Requires setting heliostat template to use for field generation
+            cp.data_set_string(r, b"solarfield.0.temp_which", b"Template 1")
+            #print( cp.generate_layout(r))  # - Poor field design
+
+        # assign
+        if True:
+            # pulling in heliostat locations
+            import csv
+            helio_data = []
+            with open('test_field.csv') as csvDataFile:
+                csvReader = csv.reader(csvDataFile)
+                for row in csvReader:
+                    helio_data.append([float(i) for i in row])
+        
+            print( cp.assign_layout(r, helio_data)) # Works
+
+            # trying soltrace simulation
+            #cp.data_set_string(r, b"fluxsim.0.flux_model", b"SolTrace")
+                    # nthreads does not work with SolTrace
+            
+            print( cp.simulate(r)) # seems to work - TODO: Check Flux map
+
+            if False:
+                res = cp.summary_results(r, save_dict=True) # Testing - DONE
+
+                #res, header = cp.detail_results(r, restype = 'matrix')  # returns (matrix, header) - DONE
+                #res = cp.detail_results(r, restype = 'matrix')  # returns (matrix, header) - DONE
+                #res = cp.detail_results(r, restype = 'dictionary')    # returns dictionary - DONE
+                #res = cp.detail_results(r, selhel = [ 3, 33, 333, 3333]) # returns dataframe - DONE
+
+
+        # update
+        if False:
+            cp.data_set_string(r, b"ambient.0.weather_file", b"C:/Users/WHamilt2/Documents/solarPILOT_build/SolarPILOT/deploy/climate_files/USA CA Daggett (TMY2).csv")
+            cp.data_set_string(r, b"solarfield.0.des_sim_detail", b"Single simulation point")
+            print( cp.update_geometry(r))
+            ## _sim_error (line 1485) SolarField.cpp -> "Representive profiles"
+            ## _heliostat object does not get set up correctly SolarField.cpp (line 3965)
+
+        # Pulling field data
+        field = cp.get_layout_info(r)
+
+        import matplotlib.pyplot as plt
+
+        x = []
+        y = []
+        for i in range(len(field)):
+            x.append(field[i][1])
+            y.append(field[i][2])
+        plt.scatter(x,y,s=0.5)
+        plt.show()
+
+    # Testing dump_varmap and save_from script - DONE
+    if False:
+        import os
+        cwd = os.getcwd()
+        print( cp.dump_varmap_tofile(r, b"C:/Users/WHamilt2/Documents/solarPILOT_build/SolarPILOT/deploy/api/varmap_dump.csv") )
+        print( cp.dump_varmap_tofile(r, b"varmap_dump_v2.csv")) # This does not work - must provide full path
+        print( cp.dump_varmap_tofile(r, (cwd + '\\varmap_dump_v3.csv').encode()) ) # This works
+
+        print( cp.save_from_script(r, b"C:/Users/WHamilt2/Documents/solarPILOT_build/SolarPILOT/deploy/api/case_study.spt"))
+        print( cp.save_from_script(r, (cwd + '\\case_study_v2.spt').encode()) ) # This works
 
 
 
