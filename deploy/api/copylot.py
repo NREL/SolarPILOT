6fc2d8be81b51847b20ac61c6cffd7857e38eecc
William Hamilton <34353104+qualand@users.noreply.github.com>
2020-06-10 09:17:13 -0500

    More work on python API - all functions are working using most conditions.  However, field generation needs more work for all simulation types other than 'do not filter'

--- deploy/api/copylot.py
+++ deploy/api/copylot.py
@@ -207,7 +207,7 @@ class CoPylot:
         # handling selected heliostats
         if selhel == None:
             nselhel = 0
-            arr = c_number(-100)
+            arr = c_number(0)
         else:
             nselhel = len(selhel)
             arr = (c_number*nselhel)()
@@ -245,20 +245,27 @@ class CoPylot:
                 df.set_index(header[0])
                 return df
 
-    #SPEXPORT bool sp_get_fluxmap(sp_data_t p_data, sp_number_t* fluxmap, int* nrows, int* ncols, int rec_id = 0)
+    #SPEXPORT sp_number_t* sp_get_fluxmap(sp_data_t p_data, int* nrows, int* ncols, int rec_id = 0)
     def get_fluxmap(self, p_data, rec_id = 0):
-        # TODO: set up function
-        pass
+        nrows = c_int()
+        ncols = c_int()
+        self.pdll.sp_get_fluxmap.restype = POINTER(c_number)
+        res = self.pdll.sp_get_fluxmap( c_void_p(p_data), byref(nrows), byref(ncols), c_int(rec_id))
+        # output matrix
+        mat = []
+        for r in range(nrows.value):
+            row = []
+            for c in range(ncols.value):
+                row.append( float(res[ncols.value * r + c]))
+            mat.append(row)
+        return mat
     
     #SPEXPORT void sp_clear_land(sp_data_t p_data, const char* type = NULL)
     def clear_land(self, p_data, clear_type = None):
-        ## TODO: Test function
-        ## TODO: Should add a bool to return if the process was successful or not
         return self.pdll.sp_clear_land( c_void_p(p_data), c_char_p(clear_type))
 
     #SPEXPORT bool sp_add_land(sp_data_t p_data, const char* type, sp_number_t* polygon_points, int* npts , int* ndim, bool is_append = true)
     def add_land(self, p_data, add_type, poly_points, is_append = True):
-        ## TODO: set-up and test function
         npts = len(poly_points)
         ndim = len(poly_points[0])
         size = npts*ndim
@@ -270,19 +277,122 @@ class CoPylot:
                 idx += 1
         
         self.pdll.sp_add_land.restype = c_bool
-        return self.pdll.sp_add_land( c_void_p(p_data), c_char_p(add_type), pointer(PParr), c_int(npts), c_int(ndim), c_bool(is_append))
+        return self.pdll.sp_add_land( c_void_p(p_data), c_char_p(add_type), pointer(PParr), byref(c_int(npts)), byref(c_int(ndim)), c_bool(is_append))
 
-    #SPEXPORT bool sp_heliostats_by_region(sp_data_t p_data, sp_number_t* retvec, int* lenret, const char* coor_sys,
-    #                                         sp_number_t* arguments = NULL, int* len_arg = NULL, 
-    #                                         const char* svgfname_data = NULL, sp_number_t* svg_opt_tab = NULL)
-    def heliostats_by_region(self, p_data):
-        # TODO: set-up and test function
-        pass
+    #SPEXPORT sp_number_t* sp_heliostats_by_region(sp_data_t p_data, const char* coor_sys, int* lenret,
+    #                                                sp_number_t* arguments = NULL, int* len_arg = NULL,
+    #                                                const char* svgfname_data = NULL, sp_number_t* svg_opt_tab = NULL);
+    def heliostats_by_region(self, p_data, coor_sys = b'all', **kwargs):
+        # TODO: test function
+        """
+        Returns heliostats that fall within a region. Options are:\n
+            >> all (no additional arguments),\n
+            >> cylindrical (provide [rmin, rmax, azmin radians, azmax radians]),\n
+            >> cartesian (provide [xmin, xmax, ymin, ymax[, zmin, zmax]]),\n
+            >> polygon (provide [[x1,y1],[x2,y2],...]),\n
+            >> svg (provide string with 'scale-x scale-y;offset-x offset-y;<svg path 1>;<svg path 2>;...',\n
+            >> svgfile (provide string filename, optional table (array) [scale-x, scale-y, offset-x, offset-y]).\n
+            (string:system, variant:region info[, string:return info - id/location])\n
+        Returns an array of included heliostat ID's and locations. : matrix, dictionary, dataframe
+        """
+        argsdict = {
+            'arguments': [],
+            'svgfname_data': b'', 
+            'svg_opt_tab' : [],
+            'restype': 'dataframe'
+        }
+        argsdict.update(kwargs)
+
+        # flatten polygon points
+        if coor_sys == b'polygon':
+            temp = []
+            for r in range(len(argsdict['arguments'])):
+                for c in range(len(argsdict['arguments'][0])):
+                    temp.append(argsdict['arguments'][r][c])
+            argsdict['arguments'] = temp
+        
+        len_arg = len(argsdict['arguments'])
+        arg_arr = (c_number*len_arg)()
+        arg_arr[:] = argsdict['arguments']
+
+        lenret = c_int()
+        self.pdll.sp_heliostats_by_region.restype = POINTER(c_number)
+        if coor_sys == b'all':
+            res = self.pdll.sp_heliostats_by_region( c_void_p(p_data), c_char_p(coor_sys), byref(lenret) )
+        elif coor_sys == b'cylindrical' or coor_sys == b'cartesian' or coor_sys == b'polygon':
+            res = self.pdll.sp_heliostats_by_region( c_void_p(p_data), c_char_p(coor_sys), byref(lenret), pointer(arg_arr), byref(c_int(len_arg)))
+        elif coor_sys == b'svg' or coor_sys == b'svgfile':
+            if len(argsdict['svg_opt_tab'] == 0):
+                res = self.pdll.sp_heliostats_by_region( c_void_p(p_data), c_char_p(coor_sys), byref(lenret), pointer(arg_arr), byref(c_int(len_arg)), c_char_p(argsdict['svgfname_data']) )
+            elif len(argsdict['svg_opt_tab']) == 4:
+                svg_tab = (c_number*4)()
+                svg_tab[:] = argsdict['svg_opt_tab']
+                res = self.pdll.sp_heliostats_by_region( c_void_p(p_data), c_char_p(coor_sys), byref(lenret), pointer(arg_arr), byref(c_int(len_arg)), c_char_p(argsdict['svgfname_data']), pointer(svg_tab) )
+            else:
+                print('svg_opt_tab must have the following form: [scale-x, scale-y, offset-x, offset-y]')
+        else:
+            print('Invalid region type specified. Expecting one of [cylindrical, cartesian, polygon, svg, svgfile]')
+
+        # Unpacking results - id, location x, location y, location z
+        header = ['id','location-x','location-y','location-z']
+        if argsdict['restype'].lower().startswith("mat"):
+            # output matrix
+            mat = []
+            for r in range(int(lenret.value/4)):
+                row = []
+                for c in range(4):
+                    row.append( float(res[4 * r + c]))
+                mat.append(row)
+            return mat, header
+        elif argsdict['restype'].lower().startswith("dic") or argsdict['restype'].lower().startswith("dat"):
+            # output dictionary
+            ret = {}
+            for c,key in enumerate(header):
+                ret[key] = []
+                for r in range(int(lenret.value/4)):
+                    ret[key].append( float(res[4 * r + c]))
+            if argsdict['restype'].lower().startswith("dic"):
+                return ret
+            else:
+                df = pd.DataFrame(ret)
+                df.set_index(header[0])
+                return df
 
     #SPEXPORT bool sp_modify_heliostats(sp_data_t p_data, sp_number_t* helio_data, int* nhel, int* ncols, const char* table_hdr)
-    def modify_heliostats(self, p_data, helio_data, table_hdr):
+    def modify_heliostats(self, p_data, helio_dict):
         # TODO: set-up and test function
-        pass
+        ## helio_data is a dictionary using a subset of the following keys
+        """
+            'id',               - Required
+            'location-x',
+            'location-y',
+            'location-z',
+            'aimpoint-x',
+            'aimpoint-y',
+            'aimpoint-z',
+            'soiling',
+            'reflectivity',
+            'enabled'
+        """
+        ncols = len(helio_dict.keys())
+        nhel = len(helio_dict['id'])
+        table_hdr = ""
+        for key in helio_dict.keys():
+            table_hdr += key
+            if (key != list(helio_dict)[-1]):
+                table_hdr += ','
+        table_hdr = table_hdr.encode()
+        
+        size = ncols*nhel
+        helio_data = (c_number*size)()
+        idx = 0
+        for h in range(nhel):
+            for key in helio_dict.keys():
+                helio_data[idx] = c_number(helio_dict[key][h])
+                idx += 1
+
+        self.pdll.sp_modify_heliostats.restype = c_bool
+        return self.pdll.sp_modify_heliostats( c_void_p(p_data), pointer(helio_data), byref(c_int(nhel)), byref(c_int(ncols)), c_char_p(table_hdr) )
 
     #SPEXPORT bool sp_save_from_script(sp_data_t p_data, const char* sp_fname)
     def save_from_script(self, p_data, sp_fname):
@@ -296,8 +406,6 @@ class CoPylot:
     
 #-----------------------------------------------------------------
 if __name__ == "__main__":
-
-    
 
     cp = CoPylot()
 
@@ -365,7 +473,7 @@ if __name__ == "__main__":
             cp.data_set_string(r, b"solarfield.0.des_sim_detail", b"Do not filter heliostats")
             ## Requires setting heliostat template to use for field generation
             cp.data_set_string(r, b"solarfield.0.temp_which", b"Template 1")
-            #print( cp.generate_layout(r))  # - Poor field design
+            print( cp.generate_layout(r))  # - Poor field design
 
         # assign
         if True:
@@ -383,47 +491,74 @@ if __name__ == "__main__":
             #cp.data_set_string(r, b"fluxsim.0.flux_model", b"SolTrace")
                     # nthreads does not work with SolTrace
             
-            print( cp.simulate(r)) # seems to work - TODO: Check Flux map
+            cp.data_set_number(r, b'fluxsim.0.x_res', 45)
+            cp.data_set_number(r, b'fluxsim.0.y_res', 30)
+            print( cp.simulate(r)) # seems to work
 
             if False:
-                res = cp.summary_results(r, save_dict=True) # Testing - DONE
+                res = cp.summary_results(r, save_dict=True)                 # Testing - DONE
+                #res, header = cp.detail_results(r, restype = 'matrix')     # returns (matrix, header) - DONE
+                #res = cp.detail_results(r, restype = 'matrix')             # returns (matrix, header) - DONE
+                #res = cp.detail_results(r, restype = 'dictionary')         # returns dictionary - DONE
+                res = cp.detail_results(r, selhel = [ 3, 33, 333, 3333])    # returns dataframe - DONE
+                res = cp.heliostats_by_region(r, coor_sys=b'polygon', arguments = [[500,500], [1000,1000], [500,1000]]) # tested all, cylindrical, cartesian, and polygon - DONE
 
-                #res, header = cp.detail_results(r, restype = 'matrix')  # returns (matrix, header) - DONE
-                #res = cp.detail_results(r, restype = 'matrix')  # returns (matrix, header) - DONE
-                #res = cp.detail_results(r, restype = 'dictionary')    # returns dictionary - DONE
-                #res = cp.detail_results(r, selhel = [ 3, 33, 333, 3333]) # returns dataframe - DONE
-
-
-        # update
-        if False:
+        # update field
+        if True:
             cp.data_set_string(r, b"ambient.0.weather_file", b"C:/Users/WHamilt2/Documents/solarPILOT_build/SolarPILOT/deploy/climate_files/USA CA Daggett (TMY2).csv")
             cp.data_set_string(r, b"solarfield.0.des_sim_detail", b"Single simulation point")
+            ##check = cp.add_land(r, b'exclus', [[1000, 1000],[500,1000], [500,500], [1000, 500]]) # - Testing - DONE
+            if True:
+                helio_dict = {}
+                helio_dict['id'] = [3,33]
+                helio_dict['location-x'] = [1500, -1500]
+                helio_dict['location-y'] = [1500, 1500]
+                helio_dict['soiling'] = [ 0.5, 0.3]
+                helio_dict['reflectivity'] = [0.2, 0.8]
+                helio_dict['enabled'] = [1, 0]
+                check = cp.modify_heliostats(r, helio_dict) # TODO: doesn't seem to modify the field
+            
+            
             print( cp.update_geometry(r))
             ## _sim_error (line 1485) SolarField.cpp -> "Representive profiles"
             ## _heliostat object does not get set up correctly SolarField.cpp (line 3965)
 
         # Pulling field data
-        field = cp.get_layout_info(r)
+        field = cp.get_layout_info(r) # Tested
+
+        #cp.clear_land(r, clear_type=b'inclusion') # Testing - DONE
 
         import matplotlib.pyplot as plt
+        # Plotting solarfield layout
+        if True:
+            x = []
+            y = []
+            for i in range(len(field)):
+                x.append(field[i][1])
+                y.append(field[i][2])
+            plt.scatter(x,y,s=0.5)
+            plt.tight_layout()
+            plt.show()
 
-        x = []
-        y = []
-        for i in range(len(field)):
-            x.append(field[i][1])
-            y.append(field[i][2])
-        plt.scatter(x,y,s=0.5)
-        plt.show()
+        flux = cp.get_fluxmap(r) # Tested
+        # Plotting flux map
+        if False:
+            im = plt.imshow(flux)
+            plt.colorbar(im)
+            plt.tight_layout()
+            plt.show()
+            
+
 
     # Testing dump_varmap and save_from script - DONE
     if False:
         import os
         cwd = os.getcwd()
         print( cp.dump_varmap_tofile(r, b"C:/Users/WHamilt2/Documents/solarPILOT_build/SolarPILOT/deploy/api/varmap_dump.csv") )
-        print( cp.dump_varmap_tofile(r, b"varmap_dump_v2.csv")) # This does not work - must provide full path
+        print( cp.dump_varmap_tofile(r, b"varmap_dump_v2.csv") ) # This does not work - must provide full path
         print( cp.dump_varmap_tofile(r, (cwd + '\\varmap_dump_v3.csv').encode()) ) # This works
 
-        print( cp.save_from_script(r, b"C:/Users/WHamilt2/Documents/solarPILOT_build/SolarPILOT/deploy/api/case_study.spt"))
+        print( cp.save_from_script(r, b"C:/Users/WHamilt2/Documents/solarPILOT_build/SolarPILOT/deploy/api/case_study.spt") )
         print( cp.save_from_script(r, (cwd + '\\case_study_v2.spt').encode()) ) # This works
 
 
